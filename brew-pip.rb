d3f09134c9733156f8ab990f34e9436fca13a001
Eric Davis <ed@npri.org>
2011-12-26 11:46:25 -0800

    brew-pip 0.3.0

--- brew-pip.rb
+++ brew-pip.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class BrewPip < Formula
-  url 'https://github.com/edavis/brew-pip/tarball/v0.2.0'
+  url 'https://github.com/edavis/brew-pip/tarball/v0.3.0'
   homepage 'https://github.com/edavis/brew-pip'
-  md5 '06698fa58becc6a53f5534fed857932b'
+  md5 'a4c3b18f21789b65543a3873550242ca'
   depends_on 'python'
 
   def install
