cce22c78df62ebef692c5ed70ff5053c9aaaed7f
Eric Davis <ed@npri.org>
2011-12-27 08:55:24 -0800

    Add test for .tar.bz2 parsing

--- tests.py
+++ tests.py
@@ -13,6 +13,7 @@ def test_get_package_info():
         ('django', ('django', '1.3.1')), # lowercase, too
         ('Django==1.2', ('django', '1.2')),
         ('./transmissionrpc-0.9.tar.gz', ('transmissionrpc', '0.9')),
+        ('./transmissionrpc-0.9.tar.bz2', ('transmissionrpc', '0.9')),
         ('transmissionrpc-0.9.tar.gz', ('transmissionrpc', '0.9')),
         ('./transmissionrpc-0.9.zip', ('transmissionrpc', '0.9')),
         ('~/src/transmissionrpc-0.9.tar.gz', ('transmissionrpc', '0.9')),
