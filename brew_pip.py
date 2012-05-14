b829f004a5f6f3039e1944b9c2ab1a75288a6cfc
Eric Davis <ed@npri.org>
2012-05-13 20:20:08 -0700

    Don't grab versions when creating prefix to install to

--- brew_pip.py
+++ brew_pip.py
@@ -1,76 +1,37 @@
 #!/usr/bin/env python
 
+"""
+Wrapper around pip to install python distributions within Homebrew.
+"""
+
+__version__ = '0.4.0'
+
 import re
 import os
-import sys
 import shutil
-import tempfile
 import argparse
-import xmlrpclib
-from pip.vcs import VcsSupport
+import tempfile
 
-VERSION = '0.4.0'
-PYPI_ENDPOINT = "http://pypi.python.org/pypi"
 HOMEBREW_CELLAR = os.environ.get("HOMEBREW_CELLAR")
-CACHE_LOCATION = os.environ.get("PIP_DOWNLOAD_CACHE") or os.environ.get("HOMEBREW_CACHE")
-VERSION_REGEXP = r'(?P<name>[\w-]+)-(?P<version>[0-9.-_]+)\.'
-ENABLED_PIP_VCS_BACKENDS = tuple(["%s+" % scheme for scheme in VcsSupport.schemes])
-
-class NoPackageInfo(Exception): pass
-
-def get_package_info(package):
-    """
-    Return (package_name, version) for a given package.
-
-    If the version isn't specified in package, query PyPI for the
-    latest.
-    """
-    name, version = None, None
-
-    if package.startswith(ENABLED_PIP_VCS_BACKENDS):
-        assert "#egg=" in package, "VCS package missing an '#egg=' identifier"
-        result = re.search('#egg=(?P<name>.+)$', package)
-        name = result.group('name')
-
-        result = re.search('@(?P<version>[^#]+)#', package)
-        if result:
-            version = "rev-" + result.group('version')
-        else:
-            version = 'HEAD'
-    elif re.search(VERSION_REGEXP, os.path.basename(package)):
-        result = re.search(VERSION_REGEXP, os.path.basename(package))
-        name = result.group('name')
-        version = result.group('version')
-    elif '==' in package:
-        name, version = package.split('==')
-    else:
-        client = xmlrpclib.ServerProxy(PYPI_ENDPOINT)
-        if '>=' in package:
-            (package, _) = package.split('>=')
-        releases = client.package_releases(package)
-        if not releases:
-            # PyPI searching is case-sensitive, so try a capitalized
-            # version of the package name if the above didn't find
-            # anything.
-            releases = client.package_releases(package.capitalize())
-            assert releases, "Couldn't find any package named '%s'" % package
-        name = package
-        version = releases[0]
-
-    if name and version:
-        return (name.lower(), version)
-    else:
-        raise NoPackageInfo("Couldn't determine either name (%r) or version (%r) "
-                            "for the requested package (%r).  Aborting." % (name, version, package))
 
 def main(args):
-    for package in args.package:
-        (package_name, version) = get_package_info(package)
+    for package in args.packages:
+        # Just the name part of the package (no version info)
+        # Django==1.4 -> Django
+        package_name = re.search(r'^(?P<name>[\w.-]+)', package).group('name')
+        assert package_name, "Invalid package name: '%s'" % package
+
+        # Directory in /usr/local/Cellar it gets installed to
+        # Django==1.4 -> pip-django
+        cellar_package_name = "pip-%s" % package_name.lower()
 
         if args.upgrade:
-            os.system("brew rm %s" % package_name)
+            os.system("brew rm %s" % cellar_package_name)
 
-        prefix = os.path.join(HOMEBREW_CELLAR, package_name, version)
+        # Why hard-code the version to "pypi"? Because if you care about
+        # versions you're probably already using virtualenv and not
+        # installing distributions site wide.
+        prefix = os.path.join(HOMEBREW_CELLAR, cellar_package_name, "pypi")
         build_dir = tempfile.mkdtemp(prefix='brew-pip-')
 
         cmd = ["pip", "install",
@@ -80,24 +41,21 @@ def main(args):
                "--install-option=--prefix=%s" % prefix,
                "--install-option=--install-scripts=%s" % os.path.join(prefix, "share", "python")]
 
-        if CACHE_LOCATION:
-            cmd.append("--download-cache=%s" % CACHE_LOCATION)
-
         if args.verbose:
-            print " ".join(cmd)
+            print(" ".join(cmd))
 
         os.system(" ".join(cmd))
 
         if not args.keg_only:
-            os.system("brew link %s" % package_name)
+            os.system("brew link %s" % cellar_package_name)
 
         shutil.rmtree(build_dir)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog='brew pip')
-    parser.add_argument("--version", action="version", version="%(prog)s v" + VERSION)
+    parser.add_argument("--version", action="version", version="%(prog)s v" + __version__)
     parser.add_argument("-v", "--verbose", action="store_true", default=False, help="be verbose")
     parser.add_argument("-k", "--keg-only", action="store_true", default=False, help="don't link files into prefix")
     parser.add_argument("-u", "--upgrade", action="store_true", default=False, help="upgrade the package")
-    parser.add_argument("package", nargs='+', help="name of the package(s) to install")
+    parser.add_argument("packages", nargs='+', help="name of the package(s) to install", metavar="package")
     main(parser.parse_args())
